6a60d169c530baedb4ea1366ecc5f7ca65b2f0ce
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-11 16:24:53 +0530

    removed the unnecessory logs

--- middleware.js
+++ middleware.js
@@ -1,12 +1,9 @@
 export const validateBook = (schema) => async (req, res, next) => {
     try {
-        console.log(schema)
         const parsedBody = await schema.parseAsync(req.body);
-        console.log(parsedBody)
         req.body = parsedBody;
         next();
     } catch (err) {
-        console.log("ha bhai ye ha jad")
         res.status(400).json({ msg: err.errors[0].message })
     }
 }
